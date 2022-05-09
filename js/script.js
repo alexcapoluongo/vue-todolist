f9215dbf55f73b8ff1833176aee94225dfed4462
alexcapoluongo <alexcapoluongo@icloud.com>
2022-05-09 15:03:53 +0200

    item done line through class added

--- js/script.js
+++ js/script.js
@@ -6,6 +6,9 @@
 // MILESTONE 3
 // Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 
+// 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+// 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+
 const app = new Vue (
     {
         el: "#root",
