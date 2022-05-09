95de6894b1d72f686530f942ab273590b45a2437
alexcapoluongo <alexcapoluongo@icloud.com>
2022-05-09 14:29:29 +0200

    mileston 1 added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,43 @@
+// MILESTONE 1
+// Stampare all'interno di una lista, un item per ogni todo.
+// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+// MILESTONE 2
+// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+// MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+
 const app = new Vue (
     {
         el: "#root",
 
         data: {
+            todo: [
+                {
+                    text: 'Work on the project',
+                    done: false
+                },
+                {
+                    text: 'Add something in Portfolio',
+                    done: false
+                },
+                {
+                    text: 'Have a meet with Sarah',
+                    done: false
+                },
+                {
+                    text: 'Join a new online language class',
+                    done: false
+                },
+                {
+                    text: 'Read 20 minutes',
+                    done: false
+                },
+                {
+                    text: 'Run 10kms',
+                    done: false
+                }
 
+            ]
         },
 
         methods: {
